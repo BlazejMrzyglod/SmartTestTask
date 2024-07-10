38f8dd7896d9ce68c6327aa73c37a3cd46f421b2
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2024-07-10 16:10:55 +0200

    Create DatabaseSeeding.sql

---
+++ OutOfOfficeDatabaseScripts/OutOfOfficeDatabaseScripts/DatabaseSeeding.sql
@@ -0,0 +1,28 @@
+USE OutOfOffice;
+INSERT INTO Employees (FullName, Subdivision, Position, Status, PeoplePartner, OutOfOfficeBalance)
+VALUES ('John Doe', 'HR', 'HR Manager', 'Active', 1, 30);
+
+INSERT INTO Employees (FullName, Subdivision, Position, Status, PeoplePartner, OutOfOfficeBalance)
+VALUES ('Diana Lynch', 'HR', 'HR Manager', 'Active', 1, 30);
+
+UPDATE Employees
+SET PeoplePartner=2
+WHERE ID=1;
+
+INSERT INTO Employees (FullName, Subdivision, Position, Status, PeoplePartner, OutOfOfficeBalance)
+VALUES ('Harrison Taylor', 'Development', 'Developer', 'Active', 2, 30);
+
+INSERT INTO Employees (FullName, Subdivision, Position, Status, PeoplePartner, OutOfOfficeBalance)
+VALUES ('Deborah Morris', 'Management', 'Project Manager', 'Active', 2, 30);
+
+INSERT INTO Employees (FullName, Subdivision, Position, Status, PeoplePartner, OutOfOfficeBalance)
+VALUES ('Ila Salazar', 'Administration', 'Administrator', 'Active', 1, 30);	
+
+INSERT INTO LeaveRequests (Employee, AbscenceReason, StartDate, EndDate)
+VALUES (3, 'Sickness', GETDATE(), GETDATE()+3);	
+
+INSERT INTO ApprovalRequests (Approver, LeaveRequest)
+VALUES (1, 1);	
+
+INSERT INTO Projects (ProjectType, StartDate, ProjectManager, Status)
+VALUES ('Backend', GETDATE(), 4, 'Active');

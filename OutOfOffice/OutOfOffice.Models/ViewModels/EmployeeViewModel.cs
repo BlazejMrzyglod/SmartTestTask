0561e4ded2a681167abe506d946de785252b2d91
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2024-07-11 21:13:32 +0200

    Create EmployeeViewModel.cs

---
+++ OutOfOffice/OutOfOffice.Models/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfOffice.Models.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+
+        public string Subdivision { get; set; } = null!;
+
+        public string Position { get; set; } = null!;
+
+        public string Status { get; set; } = null!;
+
+        public string PeoplePartner { get; set; } = null!;
+
+        public int OutOfOfficeBalance { get; set; }
+
+        public byte[]? Photo { get; set; }
+    }
+}

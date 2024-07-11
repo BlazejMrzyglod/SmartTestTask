69e59be6c164e0c9848c9f5648c3e7c9c556f439
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2024-07-11 14:40:52 +0200

    Created necessary directories and controllers

---
+++ OutOfOffice/OutOfOffice/Controllers/Lists/ApprovalRequests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OutOfOffice.Controllers.Lists
+{
+	public class ApprovalRequests : Controller
+	{
+		// GET: ApprovalRequests
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		// GET: ApprovalRequests/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: ApprovalRequests/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: ApprovalRequests/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: ApprovalRequests/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: ApprovalRequests/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: ApprovalRequests/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: ApprovalRequests/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
---
+++ OutOfOffice/OutOfOffice/Controllers/Lists/LeaveRequests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OutOfOffice.Controllers.Lists
+{
+	public class LeaveRequests : Controller
+	{
+		// GET: LeaveRequests
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		// GET: LeaveRequests/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: LeaveRequests/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: LeaveRequests/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: LeaveRequests/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: LeaveRequests/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: LeaveRequests/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: LeaveRequests/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}

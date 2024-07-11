53fb5f6b1405c7bd1dd619d88d5546a78420fe5c
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2024-07-11 18:08:09 +0200

    Implemented employee list sorting

--- OutOfOffice/OutOfOffice/Controllers/Lists/Employees.cs
+++ OutOfOffice/OutOfOffice/Controllers/Lists/Employees.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OutOfOffice.Models;
 using OutOfOffice.Services.Data;
 using OutOfOffice.Services.Repository;
@@ -18,10 +19,63 @@ namespace OutOfOffice.Controllers.Lists
 		}
 
 		// GET: Employees
-		public ActionResult Index()
+/*		public ActionResult Index()
 		{
 			IQueryable<Employee> employees = _repository.GetAllRecords();
 			return View(employees);
+		}*/
+
+		public async Task<IActionResult> Index(string sortOrder)
+		{
+			ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+			ViewData["SubdivisionSortParm"] = sortOrder == "Subdivision" ? "subdivision_desc" : "Subdivision";
+			ViewData["PositionSortParm"] = sortOrder == "Position" ? "position_desc" : "Position";
+			ViewData["PartnerSortParm"] = sortOrder == "Partner" ? "partner_desc" : "Partner";
+			ViewData["BalanceSortParm"] = sortOrder == "Balance" ? "balance_desc" : "Balance";
+			ViewData["StatusSortParm"] = sortOrder == "Status" ? "status_desc" : "Status";
+
+			IQueryable<Employee> employees = _repository.GetAllRecords();
+
+			switch (sortOrder)
+			{
+				case "name_desc":
+					employees = employees.OrderByDescending(e => e.FullName);
+					break;
+				case "Subdivision":
+					employees = employees.OrderBy(e => e.Subdivision);
+					break;
+				case "subdivision_desc":
+					employees = employees.OrderByDescending(e => e.Subdivision);
+					break;
+				case "Position":
+					employees = employees.OrderBy(e => e.Position);
+					break;
+				case "position_desc":
+					employees = employees.OrderByDescending(e => e.Position);
+					break;
+				case "Partner":
+					employees = employees.OrderBy(e => e.PeoplePartner);
+					break;
+				case "partner_desc":
+					employees = employees.OrderByDescending(e => e.PeoplePartner);
+					break;
+				case "Balance":
+					employees = employees.OrderBy(e => e.OutOfOfficeBalance);
+					break;
+				case "balance_desc":
+					employees = employees.OrderByDescending(e => e.OutOfOfficeBalance);
+					break;
+				case "Status":
+					employees = employees.OrderBy(e => e.Status);
+					break;
+				case "status_desc":
+					employees = employees.OrderByDescending(e => e.Status);
+					break;
+				default:
+					employees = employees.OrderBy(e => e.FullName);
+					break;
+			}
+			return View(await employees.AsNoTracking().ToListAsync());
 		}
 
 		// GET: Employees/Details/5

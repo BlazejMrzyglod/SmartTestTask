8a9c2c7cabd2b95dbd094b9604243f20a6f6ae78
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2024-07-16 19:08:45 +0200

    Added approval request view model

---
+++ OutOfOffice/OutOfOffice.Models/ViewModels/ApprovalRequestViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfOffice.Models.ViewModels
+{
+    public class ApprovalRequestViewModel
+    {
+        public int Id { get; set; }
+
+        public string Approver { get; set; } = null!;
+
+		public int LeaveRequest { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        public string? Comment { get; set; }
+		public static SelectList StatusOptions { get; set; } = new SelectList(new List<string> { "New", "Approved", "Rejected" });
+	}
+}

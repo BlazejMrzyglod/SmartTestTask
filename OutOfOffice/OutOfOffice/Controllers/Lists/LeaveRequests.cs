9cca12496d574b408d755d83ddd6c08daaeead50
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2024-07-17 16:53:35 +0200

    Implemented list of leave requests

--- OutOfOffice/OutOfOffice/Controllers/Lists/LeaveRequests.cs
+++ OutOfOffice/OutOfOffice/Controllers/Lists/LeaveRequests.cs
@@ -1,14 +1,110 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OutOfOffice.Models;
+using OutOfOffice.Models.ViewModels;
+using OutOfOffice.Services.Data;
+using OutOfOffice.Services.Repository;
+using OutOfOffice.Services.Repository.EntityFramework;
 
 namespace OutOfOffice.Controllers.Lists
 {
 	public class LeaveRequests : Controller
 	{
-		// GET: LeaveRequests
-		public ActionResult Index()
+		private readonly IRepositoryService<LeaveRequest> _repository;
+		private readonly IMapper _mapper;
+
+		public LeaveRequests(ApplicationDbContext context, IMapper mapper)
 		{
-			return View();
+			_repository = new RepositoryService<LeaveRequest>(context);
+			_mapper = mapper;
+		}
+
+		// GET: LeaveRequests
+		public ActionResult Index(string sortOrder, int searchString, string employeeFilter, string reasonFilter, string statusFilter)
+		{
+			ViewData["IdSortParm"] = String.IsNullOrEmpty(sortOrder) ? "id_desc" : "";
+			ViewData["EmployeeSortParm"] = sortOrder == "Employee" ? "employee_desc" : "Employee";
+			ViewData["ReasonSortParm"] = sortOrder == "Reason" ? "reason_desc" : "Reason";
+			ViewData["StartDateSortParm"] = sortOrder == "StartDate" ? "startdate_desc" : "StartDate";
+			ViewData["EndDateSortParm"] = sortOrder == "EndDate" ? "enddate_desc" : "EndDate";
+			ViewData["StatusSortParm"] = sortOrder == "Status" ? "status_desc" : "Status";
+			ViewData["NumberFilter"] = searchString;
+			ViewData["EmployeeFilter"] = employeeFilter;
+			ViewData["ReasonFilter"] = reasonFilter;
+			ViewData["StatusFilter"] = statusFilter;
+
+
+			IQueryable<LeaveRequest> leaveRequests = _repository.GetAllRecords().Include(e => e.EmployeeNavigation);
+
+			if (searchString != 0)
+			{
+				leaveRequests = leaveRequests.Where(s => s.Id.Equals(searchString));
+			}
+
+			if (!String.IsNullOrEmpty(employeeFilter))
+			{
+				leaveRequests = leaveRequests.Where(s => s.EmployeeNavigation.FullName.Contains(employeeFilter));
+			}
+
+			if (!String.IsNullOrEmpty(reasonFilter))
+			{
+				leaveRequests = leaveRequests.Where(s => s.AbscenceReason.Equals(reasonFilter));
+			}
+
+			if (!String.IsNullOrEmpty(statusFilter))
+			{
+				leaveRequests = leaveRequests.Where(s => s.Status.Equals(statusFilter));
+			}
+
+			List<LeaveRequestViewModel> viewModels = new();
+
+			switch (sortOrder)
+			{
+				case "id_desc":
+					leaveRequests = leaveRequests.OrderByDescending(e => e.Id);
+					break;
+				case "Employee":
+					leaveRequests = leaveRequests.OrderBy(e => e.EmployeeNavigation.FullName);
+					break;
+				case "employee_desc":
+					leaveRequests = leaveRequests.OrderByDescending(e => e.EmployeeNavigation.FullName);
+					break;
+				case "Reason":
+					leaveRequests = leaveRequests.OrderBy(e => e.AbscenceReason);
+					break;
+				case "reason_desc":
+					leaveRequests = leaveRequests.OrderByDescending(e => e.AbscenceReason);
+					break;
+				case "StartDate":
+					leaveRequests = leaveRequests.OrderBy(e => e.StartDate);
+					break;
+				case "startdate_desc":
+					leaveRequests = leaveRequests.OrderByDescending(e => e.StartDate);
+					break;
+				case "EndDate":
+					leaveRequests = leaveRequests.OrderBy(e => e.EndDate);
+					break;
+				case "enddate_desc":
+					leaveRequests = leaveRequests.OrderByDescending(e => e.EndDate);
+					break;
+				case "Status":
+					leaveRequests = leaveRequests.OrderBy(e => e.Status);
+					break;
+				case "status_desc":
+					leaveRequests = leaveRequests.OrderByDescending(e => e.Status);
+					break;
+				default:
+					leaveRequests = leaveRequests.OrderBy(e => e.Id);
+					break;
+			}
+			foreach (var request in leaveRequests)
+			{
+				viewModels.Add(_mapper.Map<LeaveRequestViewModel>(request));
+			}
+
+			return View(viewModels);
 		}
 
 		// GET: LeaveRequests/Details/5

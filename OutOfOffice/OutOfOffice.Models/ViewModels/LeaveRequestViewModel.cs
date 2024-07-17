82b3ee3ecedd3dc32b23d3af20e18e77b385b9b5
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2024-07-17 16:51:35 +0200

    Added leave request view model

---
+++ OutOfOffice/OutOfOffice.Models/ViewModels/LeaveRequestViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OutOfOffice.Models.ViewModels
+{
+	public class LeaveRequestViewModel
+	{
+		public int Id { get; set; }
+
+		public string Employee { get; set; } = null!;
+
+		public string AbscenceReason { get; set; } = null!;
+		public static SelectList AbscenceReasonOptions { get; set; } = new SelectList(new List<string> { "Sickness", "Vacation", "Family Emergency", "Other" });
+
+		public DateOnly StartDate { get; set; }
+
+		public DateOnly EndDate { get; set; }
+
+		public string? Comment { get; set; }
+
+		public string Status { get; set; } = null!;
+		public static SelectList StatusOptions { get; set; } = new SelectList(new List<string> { "New", "Approved", "Rejected", "Canceled", "Submitted" });
+
+	}
+}
